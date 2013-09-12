4101d2197dd969591394ffc3cf3193715f06003d
Krzysztof Kaminski <krzysztof.teonte@gmail.com>
2013-09-12 12:23:33 +0200

    Authorize guards added

--- module/TeAdmin/config/module.config.php
+++ module/TeAdmin/config/module.config.php
@@ -70,5 +70,67 @@ return array(
                 )
             )
         )
-    )
+    ),
+    'zfcuser' => array(
+        // telling ZfcUser to use our own class
+        'user_entity_class' => 'TeAdmin\Entity\User',
+        // telling ZfcUserDoctrineORM to skip the entities it defines
+        'enable_default_entities' => false,
+    ),
+    'bjyauthorize' => array(
+        // Using the authentication identity provider, which basically reads the roles from the auth service's identity
+        'identity_provider' => 'BjyAuthorize\Provider\Identity\AuthenticationIdentityProvider',
+        'role_providers' => array(
+            // using an object repository (entity repository) to load all roles into our ACL
+            'BjyAuthorize\Provider\Role\ObjectRepositoryProvider' => array(
+                'object_manager' => 'doctrine.entitymanager.orm_default',
+                'role_entity_class' => 'TeAdmin\Entity\Role',
+            ),
+        ),
+        // Guard listeners to be attached to the application event manager
+        'guards' => array(
+            'BjyAuthorize\Guard\Controller' => array(
+                array(
+                    'controller' => 'TeAdmin\Controller\Index',
+                    'action' => 'index',
+                    'roles' => array('guest','user','admin'),
+                ),
+                array(
+                    'controller' => 'zfcuser',
+                    'action' => 'index',
+                    'roles' => array('guest','user','admin'),
+                ),
+                array(
+                    'controller' => 'zfcuser',
+                    // no action specified - all actions allowed!
+                    'action' => 'changepassword',
+                    'roles' => array('user','admin'),
+                ),
+                array(
+                    'controller' => 'zfcuser',
+                    // no action specified - all actions allowed!
+                    'action' => 'changeemail',
+                    'roles' => array('user','admin'),
+                ),
+                array(
+                    'controller' => 'zfcuser',
+                    // no action specified - all actions allowed!
+                    'action' => 'register',
+                    'roles' => array('guest','user','admin'),
+                ),
+                array(
+                    'controller' => 'zfcuser',
+                    // no action specified - all actions allowed!
+                    'action' => 'login',
+                    'roles' => array('guest','user','admin'),
+                ),
+                array(
+                    'controller' => 'zfcuser',
+                    // no action specified - all actions allowed!
+                    'action' => 'logout',
+                    'roles' => array('guest','user','admin'),
+                ),
+            ),
+        ),
+    ),
 );
